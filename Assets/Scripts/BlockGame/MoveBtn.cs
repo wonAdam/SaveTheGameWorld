bd8e078caffcec2ab1a9fe98407d31b9eaae5aa7
Won Dong Hyun <ehdgus5500@naver.com>
2021-10-09 03:36:49 +0000

    Animation추가

--- Assets/Scripts/BlockGame/MoveBtn.cs
+++ Assets/Scripts/BlockGame/MoveBtn.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class MoveBtn : MonoBehaviour
 {
     [SerializeField] Bar bar;
+    [SerializeField] Animator animator;
 
     public void RightBtn()  //  바 오른쪽으로 움직이기
     {
         bar.vec_bar = 1;
+        animator.SetInteger("state", 1);
     }
     public void LeftBtn()   //  바 왼쪽으로 움직이기
     {
         bar.vec_bar = -1;
-
+        animator.SetInteger("state", 1);
     }
     public void stopBtn()   //  바 멈춰!
     {
         bar.vec_bar = 0;
+        animator.SetInteger("state", 0);
     }
 }
